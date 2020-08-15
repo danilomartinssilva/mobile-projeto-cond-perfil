d1ebe4f66b38aa128b66a3ac199b2e6dc42377f2
Danilo Martins <danilomartins@MacBook-Pro-de-Danilo.local>
2020-08-15 17:01:23 -0300

    Instalacao RN Unimodules

--- src/screens/Eventos/Create/index.js
+++ src/screens/Eventos/Create/index.js
@@ -1,28 +1,29 @@
-import React, {useLayoutEffect} from 'react'
-import {View, Text} from 'react-native'
-import {Container, TInput} from './styles'
-import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import {Formik} from 'formik'
+import React, {useLayoutEffect} from 'react';
+import {View, Text} from 'react-native';
+import {Container, TInput} from './styles';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Formik} from 'formik';
 
-export default function EventosCreateScreen ({navigation}) {
+export default function EventosCreateScreen({navigation}) {
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Adicionar Evento',
       headerLeft: () => (
         <MaterialIcons
-          name='menu'
+          name="menu"
           size={28}
           style={{margin: 8, paddingRight: 10}}
           onPress={() => navigation.openDrawer()}
         />
       ),
-    })
-  }, [navigation])
+    });
+  }, [navigation]);
   return (
     <Formik>
       <Container>
-        <TInput label='Descricao' />
+        <TInput label="Descricao" />
+        <TInput label="Descricao" />
       </Container>
     </Formik>
-  )
+  );
 }
